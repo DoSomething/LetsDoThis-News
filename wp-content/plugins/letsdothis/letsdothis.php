537f988a30814f36ea253831fc1fd00af323000b
Aaron Schachter <aschachter@dosomething.org>
2016-01-08 22:46:54 +0000

    Adds Pods, JSON API, and LetsDoThis plugins

---
+++ wp-content/plugins/letsdothis/letsdothis.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package LetsDoThis
+ * @version 1.0
+ */
+
+/*
+Plugin Name: Let's Do This
+Description: Custom plugin for the Let's Do This app.
+Author: DoSomething.org
+Version: 1.0
+*/
+
+function letsdothis_allow_origin() {
+    header("Access-Control-Allow-Origin: *");
+}
+add_action( 'init', 'letsdothis_allow_origin' );
+
+?>
